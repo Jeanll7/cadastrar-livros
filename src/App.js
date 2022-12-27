a8666c7dff1d72e11980d7f75ca656868f4a5296
devjean <jeancarloleall@gmail.com>
2022-12-27 08:05:31 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+import "./App.css";
 
 function App() {
+  const [codigo, setCodigo] = useState("");
+  const [titulo, setTitulo] = useState("");
+  const [autor, setAutor] = useState("");
+  const [livros, setLivros] = useState([]);
+
+  function cadastrar() {
+    let livro = {
+      codigo,
+      titulo,
+      autor,
+    };
+    setLivros([livro, ...livros]);
+    limparForm();
+  }
+
+  function excluir(codigo) {
+    livros.forEach((livro, index) => {
+      if (livro.codigo === codigo) {
+        livros.splice(index, 1);
+        setLivros([...livros]);
+      }
+    });
+  }
+
+  function generatePassword() {
+    return Math.floor(Math.random() * 30 + 1);
+  }
+
+  function limparForm() {
+    setCodigo("");
+    setTitulo("");
+    setAutor("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Cadastrar Livros</h1>
+      <div className="container">
+        {/* <div className="inputArea">
+          <p>Código</p>
+          <input
+            onChange={(e) => {
+              setCodigo(e.target.value);
+            }}
+            type="text"
+            name="Código"
+            placeholder="###"
+            disabled
+          />
+        </div> */}
+        <div className="inputArea">
+          <p>Título</p>
+          <input
+            value={titulo}
+            onChange={(e) => {
+              setTitulo(e.target.value);
+            }}
+            type="text"
+            name="Título"
+            placeholder="título"
+          />
+        </div>
+        <div className="inputArea">
+          <p>Autor</p>
+          <input
+            value={autor}
+            onChange={(e) => {
+              setAutor(e.target.value);
+            }}
+            type="text"
+            name="Autor"
+            placeholder="autor"
+          />
+        </div>
+        <div>
+          <button onClick={cadastrar} type="submit">
+            SALVAR
+          </button>
+        </div>
+      </div>
+      <table className="table1">
+        <thead>
+          <tr>
+            <th>codigo</th>
+            <th>titulo</th>
+            <th>autor</th>
+            <th>Ação</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {livros.map((livro, codigo) => {
+            return (
+              <tr>
+                <td>{generatePassword(livro.codigo)}</td>
+                <td>{livro.titulo}</td>
+                <td>{livro.autor}</td>
+                <td>
+                  <button
+                    className="icon-trash"
+                    onClick={() => {
+                      excluir(livro.codigo);
+                    }}
+                  >
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <table className="table2">
+        <thead>
+          <tr>
+            <th>Total de livros cadastrados: {}</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
